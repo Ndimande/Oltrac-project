f32b47e45c65b727bc9d8ef1c2761da37e84f90b
Werner Roets <cobolt.exe@gmail.com>
2019-11-12 15:40:13 +0200

    added more species

--- lib/framework/model.dart
+++ lib/framework/model.dart
@@ -1,20 +1,32 @@
+import 'package:uuid/uuid.dart';
+
 abstract class Model {
-  Model();
+  /// The unique identifier of the model
+  final String uuid;
 
-  Model.fromMap(Map data);
+  /// Default constructor creates a uuid
+  Model() : uuid = Uuid().v1();
 
+  /// Construct from map. Map must contain UUID.
+  Model.fromMap(Map data) : uuid = data['uuid'];
+
+  /// Create a copy with changes
   Model copyWith();
 
+  /// Get as a map
   Map<String, dynamic> toMap();
 
+  /// Get the model as a string
   @override
   String toString() => this.toMap().toString();
 
+  /// Override equality operator to use uuid.
   @override
   bool operator ==(other) {
-    return other.toString() == this.toString();
+    return other.uuid == uuid;
   }
 
+  /// Get hash code from uuid
   @override
-  int get hashCode => this.toString().hashCode;
+  int get hashCode => uuid.hashCode;
 }
